951f12eb5b5ec0aaa786fb15fd1985a796e64fa9
AbdHou <abdhou@proton.me>
2026-05-10 20:11:28 +0200

    init project

---
+++ backend/src/main/java/app/abdhou/SpringBatchExplorerApplication.java
@@ -0,0 +1,13 @@
+package app.abdhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchExplorerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringBatchExplorerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/app/abdhou/SpringBatchExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.abdhou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
